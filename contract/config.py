3943ad30c08609df17de84281cb0435564afa760
wyf-ACCEPT <define_wifi@163.com>
2023-01-11 19:30:16 +0800

    init

---
+++ contract/config.py
@@ -0,0 +1,14 @@
+class ConfigParams:
+    
+    def __init__(self):
+        pass
+    
+    MIN_BOND_TIME_PERIOD = 3600         # 1 hour
+    MAX_BOND_TIME_PERIOD = 7200         # 2 hours
+    LOCK_TIME_PERIOD = 1200             # 20 minutes
+    SHORT_COIN_TYPE = 0x011b            # Algorand SLIP44: 	0x8000011b
+            # See https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+    
+    
+    
+    
