d9af47c0ee32b06d974af38c138225a4bc9ab719
wyf-ACCEPT <define_wifi@163.com>
2023-02-10 22:15:48 +0800

    update mesonswap.py

--- contract/MesonSwap.py
+++ contract/MesonSwap.py
@@ -8,22 +8,27 @@ from MesonHelpers import *
 
 def postSwap(
     encodedSwap: Bytes,
-    r: Bytes,
-    s: Bytes,
+    r_s: Int,
     v: Int,
     postingValue: Bytes,
 ) -> Int:
     amount = itemFrom('amount', encodedSwap)
     delta = itemFrom('expireTs', encodedSwap) - Txn.first_valid_time()
+    initiator = itemFromPosted('initiator', postingValue)
+    lp = itemFromPosted('lp', postingValue)
     
     conditions = And(
         amount < cp.MAX_SWAP_AMOUNT,
         delta > cp.MIN_BOND_TIME_PERIOD,
         delta < cp.MAX_BOND_TIME_PERIOD,
+        lp == Txn.sender(),
+        checkRequestSignature(encodedSwap, r_s, v, initiator),
     )
+    
+    # todo: transferFrom?
     
     return Seq(
         Assert(conditions),
-        Assert()
-        
+        Assert(App.box_create(encodedSwap, Int(65))),
+        App.box_put(encodedSwap, postingValue),       
     )
